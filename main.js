d684405060e75cb796b63bf956084292f4060d45
lamkhai0604 <38321198+lamkhai0604@users.noreply.github.com>
2020-05-07 22:40:19 +0700

    add more animation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let i = 0;
 let txt = 'Nowadays information technology is growing faster than ever with the needs of humans in a developing world. Therefore, it acquired people to learn and self-improve to catch the flow of the century. With enthusiasm and interest in technology, I believe full-stack web development is a new challenge that I would love to take';
 let speed = 50;
-const texts = ['No one can make you feel inferior without your consent  --Eleanor Roosevelt'];
+const texts = ['If you are interested in my profile don\'t hesitate to leave me a message or contact me directly via...'];
 let count = 0;
 let index = 0;
 let currentText = '';
@@ -26,13 +26,11 @@ function typeWriter() {
 }
 
 (function type() {
-
     if (count === texts.length) {
         count = 0;
     }
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
-
     document.querySelector(".typing").textContent = letter;
     if (letter.length === currentText.length) {
         count++;
